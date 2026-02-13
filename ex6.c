cb28b3ba362f015b005973900efb22b7e3983b46
Sam Schulman <samfschulman@gmail.com>
2026-02-13 14:54:17 -0600

    Initial ex6 code

--- ex6.c
+++ ex6.c
@@ -1,5 +1,22 @@
 #include <string.h>
+#include <stdio.h>
+
+void transformUpper(char *string){
+
+    for (int i=0; i < strlen(string); i++){
+        if (string[i] >= 97 && string[i] <= 122){
+            string[i] = string[i]-32;
+        }
+    }
+}
+
+void apply_transform(void (*tform)(char *), char *string){
+    tform(string);
+}
 
 int main(){
-    
+    char string[] = "This is a sentence, hopefully a loud one.";
+    apply_transform(transformUpper, string);
+    printf("%s", string);
+    return 0;
 }
